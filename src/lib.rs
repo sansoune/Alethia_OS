c56718b9c3ba0325c8278e6c5d58a69e98f436a1
sansoune <yassine.dakhlaoui06@gmail.com>
2024-06-09 23:10:38 +0100

    implementing print and println

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 #![no_std]
 
 pub mod uefi;
+pub mod console;
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 
 
 use core::panic::PanicInfo;
-use alethia_os::uefi::{ImageHandle, SystemTable};
+use alethia_os::{console, println, uefi::{ImageHandle, SystemTable}};
 
 // #[cfg(not(test))]
 #[panic_handler]
@@ -12,26 +12,14 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+
 #[no_mangle]
 pub extern "efiapi" fn efi_main(handle: ImageHandle, system_table: *const SystemTable) {
-    let string =  "helllo world!\n\r";
-    let output_protocol = unsafe { &*(*system_table).output };
+    unsafe { console::SYSTEM_TABLE = system_table; }
 
-    for character in string.chars() {
-        let mut buffer:[u16;1] = [0];
-        let utf16 = character.encode_utf16(&mut buffer);
-        
-        (output_protocol.output_string)(output_protocol, &utf16[0]);
-        
-    }
-
-    let string_arr = ['h' as u16, 'i' as u16, '!' as u16, '\n' as u16, '\0' as u16];
+    println!("Alethia os!");
 
     
-    (output_protocol.output_string)(output_protocol, &string_arr[0]);
-    
-
-
     loop {
         
     }
