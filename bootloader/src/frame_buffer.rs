96d4d1d03c2ca04dbd600f81988959b5e250f749
sansoune <yassine.dakhlaoui06@gmail.com>
2024-07-20 23:59:05 +0100

    implementing frame buffer

--- bootloader/src/frame_buffer.rs
+++ bootloader/src/frame_buffer.rs
@@ -62,25 +62,17 @@ pub fn get_frame_buffer(system_table: &SystemTable<Boot>) -> Option<FrameBuffer>
 
     let mode_info = gop.current_mode_info();
     let mut frame_buffer = gop.frame_buffer();
-    let info = FrameBufferInfo {
-        size: frame_buffer.size(),
-        width: mode_info.resolution().0,
-        height: mode_info.resolution().1,
-        pixel_format: match mode_info.pixel_format() {
-            gop::PixelFormat::Rgb => PixelFormat::Rgb,
-            gop::PixelFormat::Bgr => PixelFormat::Bgr,
-            gop::PixelFormat::Bitmask | gop::PixelFormat::BltOnly => {
-                panic!("Bitmask and BltOnly framebuffers are not supported")
-            }
-        },
-        bytes_per_pixel: 4,
-        stride: mode_info.stride(),
+    let pixel_format = match mode_info.pixel_format() {
+        gop::PixelFormat::Rgb => PixelFormat::Rgb,
+        gop::PixelFormat::Bgr => PixelFormat::Bgr,
+        gop::PixelFormat::Bitmask | gop::PixelFormat::BltOnly => {
+            panic!("Bitmask and BltOnly framebuffers are not supported")
+        }
     };
 
-    Some(FrameBuffer {
-        base_addr: frame_buffer.as_mut_ptr(),
-        info,
-    })
+    let fb = FrameBuffer::new(frame_buffer.as_mut_ptr(), frame_buffer.size(), mode_info.resolution().0, mode_info.resolution().1, pixel_format, 4, mode_info.stride());
+
+    Some(fb)
 }
 
 pub fn write_to_frame_buffer(frame_buffer: &FrameBuffer, x: usize, y: usize, color: u32) {
