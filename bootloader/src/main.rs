909eab22d56d563606fae4a85bc3d1ca30e544ab
sansoune <yassine.dakhlaoui06@gmail.com>
2024-07-21 20:08:25 +0100

    getting upper and lower memory

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -3,7 +3,7 @@
 
 use core::panic::PanicInfo;
 use bootloader::{load_file::{load_font, load_kernel, open_file}, BootInfo};
-use uefi::{prelude::*, println, CStr16};
+use uefi::{helpers::system_table, prelude::*, println, table::boot::{MemoryDescriptor, MemoryMapIter}, CStr16};
 use uefi::table::boot::MemoryType;
 use bootloader::frame_buffer::get_frame_buffer;
 
@@ -12,6 +12,27 @@ use bootloader::frame_buffer::get_frame_buffer;
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+fn get_memory_info(memory_map: MemoryMapIter) -> (u64, u64) {
+    let mut lower_memory: u64 = 0;
+    let mut upper_memory: u64 = 0;
+
+    for dec in memory_map {
+        if dec.ty == MemoryType::CONVENTIONAL {
+            let start = dec.phys_start;
+            let end = start + dec.page_count * 4096;
+
+            if start < 0x100000 {
+                lower_memory += end.min(0x100000) - start;
+            }
+            if end > 0x100000 {
+                upper_memory += end - start.max(0x100000);
+            }
+        }
+    }
+
+    (lower_memory, upper_memory)
 }
 
 
@@ -31,7 +52,10 @@ fn main(image: Handle, mut st: SystemTable<Boot>) -> Status {
     let mut kernel_file = open_file(&st, kernel_name);
     let (kernel_add, entry_point_offset) = load_kernel(&st, &mut kernel_file);
         
-    
+    let mut memory_map_buffer = [0; 4096 * 4];
+    let memory_map = system_table().boot_services().memory_map(&mut memory_map_buffer).expect("failed to get memory map");
+    let (loawer_memory, upper_memory) = get_memory_info(memory_map.entries());
+    println!("lower: {:#x}, upper: {:#x}", loawer_memory, upper_memory);
 
     let fb = get_frame_buffer(&st).expect("couldn't load the frame buffer");
 
